20b415e11d65eed7097171085552ab982131be22
Michel Weststrate <mweststrate@gmail.com>
2016-04-16 18:34:20 +0200

    Updated readme

--- src/app/components/tags-overview.jsx
+++ src/app/components/tags-overview.jsx
@@ -5,16 +5,17 @@ import List from 'material-ui/lib/lists/list';
 import ListItem from 'material-ui/lib/lists/list-item';
 import Subheader from 'material-ui/lib/Subheader';
 
+const TagOverviewEntry = observer(({tag, viewState}) => <ListItem
+	primaryText={tag.name}
+	onClick={() => viewState.selectTag(tag) }
+	className={viewState.selection === tag ? 'selected' : null}
+/>)
+
 export const TagsOverview = observer(({tagStore, viewState}) =>
 	<List>
 		<Subheader>Tags</Subheader>
 		{ tagStore.tags.map(tag =>
-			<ListItem
-				key={tag.id}
-				primaryText={tag.name}
-				onClick={() => viewState.selectTag(tag) }
-				className={viewState.selection === tag ? 'selected' : null}
-			/>
+			<TagOverviewEntry key={tag.id} tag={tag} viewState={viewState} />
 		) }
 	</List>
 );
